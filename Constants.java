f10fbbf10f368e76d40306cee9fb53023e544273
lyn-kate <katelyn.ma128@gmail.com>
2026-05-04 12:09:39 -0400

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,19 @@
+/**
+ * Final Project Assignment: Constant variables (Katelyn)
+ * @author Katelyn and Amanda
+ * @version Dec 2022
+ */
+
+public final class Constants{
+    public static final int WIDTH = 1000 + 17;
+    public static final int HEIGHT = 600 + 47;
+    public static final int FRAME_PERIOD = 70;
+    public static final int FISH_SHIFT_EASY = 10;
+    public static final int FISH_SHIFT_MEDIUM = 20;
+    public static final int FISH_SHIFT_HARD = 30;
+    public static final int PENGUIN_SHIFT = 30;
+    
+
+    private Constants(){
+    }
+}
